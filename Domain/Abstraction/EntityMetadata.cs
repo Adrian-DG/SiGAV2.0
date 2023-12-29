58d44cce456fa608baf1b15d0c09df5e73c3ee62
Adrian-DG <adrian96guilamo@gmail.com>
2023-12-28 23:06:48 -0400

    minor change on EntityMetadata

--- Domain/Abstraction/EntityMetadata.cs
+++ Domain/Abstraction/EntityMetadata.cs
@@ -14,18 +14,14 @@ namespace Domain.Abstraction
         public int Id { get; set; }
 
         [DataType(DataType.DateTime)]
-        public DateTime FechaCreacion 
-        { 
-            get => this.FechaCreacion;
-            set => FechaCreacion = GetActualDateTime(); 
-        }
+        public DateTime FechaCreacion { get; set; }
 
         [DataType(DataType.DateTime)]
         public DateTime FechaModificacion { get; set; }
         public bool Estatus { get; set; } = true;
         public bool IsDeleted { get; set; } = false;
 
-        public DateTime GetActualDateTime() => DateTime.UtcNow;
+        public void ChangeVisibility() => IsDeleted = !IsDeleted;
 
     }
 }
