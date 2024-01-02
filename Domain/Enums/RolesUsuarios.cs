8006dd77a400431a92c02143199a507b63cfd897
Adrian-DG <adrian96guilamo@gmail.com>
2024-01-02 10:57:45 -0400

    roles added

---
+++ Domain/Enums/RolesUsuarios.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Enums
+{
+	public enum RolesUsuarios 
+	{
+		ADMINISTRADOR = 1,
+		ANALISTA_S3,
+		OPERADOR_R5
+	}
+}
