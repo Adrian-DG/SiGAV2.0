a7aa113d81054e8286463ea87b300d51ea91c688
Adrian Jose Diaz Guilamo <adrian96guilamo@gmail.com>
2024-01-05 22:15:31 -0400

    change register authorization level

--- API/Controllers/AuthController.cs
+++ API/Controllers/AuthController.cs
@@ -23,7 +23,7 @@ namespace API.Controllers
 		}
 
 		[HttpPost]
-		[AllowAnonymous]
+		[Authorize]
 		[Route("regiter-user")]
 		// [ProducesResponseType(StatusCodes.Status201Created)]
 		// [ProducesResponseType(StatusCodes.Status400BadRequest)]
